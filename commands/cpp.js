d4bb400f09681398137074e99bb51aa2bfc877e2
Vectrixed <silvo34@live.nl>
2017-02-21 15:32:02 +0000

    added stuff

---
+++ commands/cpp.js
@@ -0,0 +1,37 @@
+module.exports = {
+    help: 'Evals c++',
+    func: (client, message, args) => {
+    const {
+      exec
+    } = require('child_process');
+    const fs = require('fs');
+
+    const fileTemplate = fs.readFileSync('./main.cpp.template', 'utf8');
+    let input = message.content.split(" ");
+    input.shift();
+    input = input.join(" ")
+
+    const [inMain, beforeMain] = input.split(/\/\* ?-+ ?\*\//).reverse();
+
+    fs.writeFile('main.cpp', fileTemplate.replace('${inMain}', inMain).replace('${beforeMain}', beforeMain || ''), (error) => {
+      if (error) {
+        return message.edit(`\`Input\`\n\`\`\`cpp\n${input}\n\`\`\`\`Error\`\n\`\`\`cpp\n${error.message}\n\`\`\``).catch(console.error);
+      }
+
+      exec('g++ main.cpp -o output', (error, stdout) => {
+        if (error) {
+          return message.edit(`\`Input\`\n\`\`\`cpp\n${input}\n\`\`\`\`Error\`\n\`\`\`cpp\n${error.message}\n\`\`\``).catch(console.error);
+        }
+
+        exec('./output', (error, stdout) => {
+          if (error) {
+            return message.edit(`\`Input\`\n\`\`\`cpp\n${input}\n\`\`\`\`Error\`\n\`\`\`cpp\n${error.message}\n\`\`\``).catch(console.error);
+
+          }
+
+          return message.edit(`\`Input\`\n\`\`\`cpp\n${input}\n\`\`\`\`Output\`\n\`\`\`cpp\n${stdout}\n\`\`\``).catch(console.error);
+        });
+      });
+    });
+}
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdint>
+
+using namespace std;
+
+
+
+int main() {
+    cout << "hello";
+    return 0;
+}
